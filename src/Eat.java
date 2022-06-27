15492071977ac5ae2e334c2220cd5f1ae28c1b6a
kiranznair <kiranznair2@gmail.com>
2022-06-27 11:49:34 +0530

    hereIs a simpleClass

---
+++ src/Eat.java
@@ -0,0 +1,14 @@
+public class Eat {
+        public void eatMore(boolean hungry, int amountOfFood) {
+        int roomInBelly = 5;
+        if (hungry) {
+             var timeToEat = true;
+            while (amountOfFood > 0) {
+                int amountEaten = 2;
+                 roomInBelly = roomInBelly - amountEaten;
+                amountOfFood = amountOfFood - amountEaten;
+                }
+             }
+        System.out.println(amountOfFood);
+        }
+}
