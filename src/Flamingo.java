c3975d7d81718ce33aae5d3a49c476a3b6274605
kiranznair <kiranznair2@gmail.com>
2022-06-27 11:52:40 +0530

    exampleFromOcpGuide

---
+++ src/Flamingo.java
@@ -0,0 +1,12 @@
+    public class Flamingo {
+        private String color = null;
+        public void setColor(String color) {
+            color = color;
+        }
+        public static void main(String... unused) {
+            var f = new Flamingo();
+            f.setColor("PINK");
+            System.out.print(f.color);
+        }
+    }
+
